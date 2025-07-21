54dd0b2ea0372bffda1c69ad2366b61648648d19
Pick999999 <chaikaew902@gmail.com>
2025-07-22 06:42:31 +0700

    Update index.php

--- api/index.php
+++ api/index.php
@@ -1,4 +1,17 @@
 <?php
-  echo "Vercel IN API " ;
+
+header('Access-Control-Allow-Methods: GET, POST');
+header('Access-Control-Allow-Origin: *'); 
+   ob_start();
+   ini_set('display_errors', 1);
+   ini_set('display_startup_errors', 1);
+   error_reporting(E_ALL);   
+   $data = json_decode(file_get_contents('php://input'), true);
+   if ($data) {
+     echo "Have Data Post" ;
+      print_r($data) ;
+   } else {   
+     echo "No Data USE Vercel IN API " ;
+   }
 ?>
     
