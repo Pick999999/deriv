b44e25dfd01a9b28905f1a51b576df8ee5848bdf
Pick999999 <chaikaew902@gmail.com>
2025-07-22 14:02:02 +0700

    Update index.php

--- api/index.php
+++ api/index.php
@@ -1,4 +1,12 @@
 <?php
- echo "Hello";
+ echo "Hello V2 " ;
+header('Access-Control-Allow-Methods: GET, POST');
+header('Access-Control-Allow-Origin: *'); 
+ob_start();
+
+   ini_set('display_errors', 1);
+   ini_set('display_startup_errors', 1);
+   error_reporting(E_ALL);   
+   $data = json_decode(file_get_contents('php://input'), true);
 ?>
     
