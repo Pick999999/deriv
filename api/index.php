52399b84fe5ad5e75b2e81d7b7eadfbdb30ca756
Pick999999 <chaikaew902@gmail.com>
2025-07-22 16:10:49 +0700

    Update index.php

--- api/index.php
+++ api/index.php
@@ -8,6 +8,44 @@ ob_start();
    ini_set('display_startup_errors', 1);
    error_reporting(E_ALL);   
    $data = json_decode(file_get_contents('php://input'), true);
-  echo "Hello V3 " ;
+   echo "Hello V3 " ;
+  if ($data) {
+      //if ($data['Mode'] == 'getAction') { getActionV3($data); }
+	     //if ($data['Mode'] == 'getLastAction') { getActionV3($data); }
+      return;   
+   } else {
+      //$candleData = getActionV3($data='');
+      $candleData = getCandleData2();
+      echo ' Len= ' . len($candleData) ;
+   }
+
+function getCandleData2() {
+
+ $newUtilPath = '/home/thepaper/domains/thepapers.in/private_html/deriv/newDerivObject/';
+ $sFileName =  $newUtilPath.'rawData.json';
+ $st = '';
+ $file = fopen($sFileName,"r");
+ while(! feof($file))  {
+   $st .= fgets($file) ;
+ }
+ fclose($file); 
+ $candleDataA = JSON_DECODE($st,true);
+
+ for ($i=0;$i<=60;$i++) {
+	 $candleDataB[] = $candleDataA[$i] ;
+ }
+ 
+
+ 
+ echo 'Len=' . count($candleDataB) . '<br>';
+ return $candleDataB ;
+
+ 
+ 
+
+} // end function
+
+
+  
 ?>
     
