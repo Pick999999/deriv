43258be1c711c3aff644405ce8fbfd884b32563b
Pick999999 <chaikaew902@gmail.com>
2025-07-22 16:14:20 +0700

    Update index.php

--- api/index.php
+++ api/index.php
@@ -21,7 +21,7 @@ ob_start();
 
 function getCandleData2() {
 
- $newUtilPath = '../../newDerivObject/';
+ $newUtilPath = '../newDerivObject/';
  $sFileName =  $newUtilPath.'rawData.json';
  $st = '';
  $file = fopen($sFileName,"r");
